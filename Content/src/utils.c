06ad2a115b7b617cea2dcfa956eccb8a17820b82
Roland Ebrecht <roland.ebrecht@hs-augsburg.com>
2022-12-27 18:08:19 +0100

    First version with makefile and armv7-a

---
+++ Content/src/utils.c
@@ -0,0 +1,44 @@
+#include "../include/common.h"
+
+int __attribute__((noinline))
+memcmp(const void *str1, const void *str2, size_t count) {
+  const unsigned char *s1 = (const unsigned char *)str1;
+  const unsigned char *s2 = (const unsigned char *)str2;
+
+  while (count-- > 0) {
+    if (*s1++ != *s2++)
+      return s1[-1] < s2[-1] ? -1 : 1;
+  }
+
+  return 0;
+}
+
+void __attribute__((noinline))
+memcpy(void *dst, const void *src, size_t count) {
+  unsigned char *d = (unsigned char *)dst;
+  const unsigned char *s = (const unsigned char *)src;
+
+  while (count-- > 0) {
+    *d++ = *s++;
+  }
+}
+
+void __attribute__((noinline)) memset(void *dst, uint8_t val, size_t count) {
+  unsigned char *d = (unsigned char *)dst;
+
+  while (count-- > 0) {
+    *d++ = val;
+  }
+}
+
+void __attribute__((noinline)) serial_putc(char c) {
+  *(char *)UART_OUT_BUF_ADDR = c;
+}
+
+void serial_puts(char *s) {
+  while (*s) {
+    serial_putc(*s);
+
+    s++;
+  }
+}
