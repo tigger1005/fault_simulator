1861ee8c0e09a4e68f3ab951817357af878a8b13
REbrecht <roland.ebrecht@gmail.com>
2023-01-17 15:38:13 +0100

    Moved callback to sub file

--- src/simulation.rs
+++ src/simulation.rs
@@ -1,3 +1,6 @@
+mod callback;
+use callback::*;
+
 use log::debug;
 
 use unicorn_engine::unicorn_const::{
@@ -486,66 +489,4 @@ impl<'a> Simulation<'a> {
             )
             .expect("failed to set memory hook");
     }
-}
-
-/// Callback for auth mem IO write access
-///
-/// This IO call signalize the Successful or Failed boot flow
-fn mmio_auth_write_callback<D>(
-    emu: &mut Unicorn<SimulationData>,
-    _mem_type: MemType,
-    _address: u64,
-    _size: usize,
-    value: i64,
-) -> bool {
-    match value {
-        1 => {
-            emu.get_data_mut().state = RunState::Success;
-            debug!("Indicator: __SET_SIM_SUCCESS()")
-        }
-        2 => {
-            emu.get_data_mut().state = RunState::Failed;
-            debug!("Indicator: __SET_SIM_FAILED()")
-        }
-        _ => {
-            emu.get_data_mut().state = RunState::Error;
-            debug!("Indicator: Wrong_Value")
-        }
-    }
-    emu.emu_stop().expect("failed to stop");
-    true
-}
-
-/// Callback for serial mem IO write access
-///
-/// This IO write displays printed messages
-fn mmio_serial_write_callback<D>(
-    emu: &mut Unicorn<SimulationData>,
-    _address: u64,
-    _size: usize,
-    value: u64,
-) {
-    if emu.get_data().print_output {
-        print!("{}", value as u8 as char);
-    }
-}
-
-/// Hook for flash_load_img callback handling.
-fn hook_code_flash_load_img_callback<D>(
-    emu: &mut Unicorn<SimulationData>,
-    _address: u64,
-    _size: u32,
-) {
-    if emu.get_data_mut().is_positiv {
-        // Write flash data to boot stage memory
-        let boot_stage: [u8; 4] = [0x78, 0x56, 0x34, 0x12];
-        emu.mem_write(BOOT_STAGE, &boot_stage)
-            .expect("failed to write boot stage data");
-    } else {
-        // Write flash data to boot stage memory
-        let boot_stage: [u8; 4] = [0xB8, 0x45, 0x85, 0xFD];
-        emu.mem_write(BOOT_STAGE, &boot_stage)
-            .expect("failed to write boot stage data");
-    }
-    debug!("Call of flash_load_img");
 }
