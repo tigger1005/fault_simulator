a8d30fb12272c76cb495f861c307e68174ad8d70
REbrecht <roland.ebrecht@gmail.com>
2023-02-17 13:25:22 +0100

    Removed unnesessary files

--- Content/src/main.c
+++ Content/src/main.c
@@ -1,31 +1,9 @@
-#include "../include/common.h"
-#include "../include/utils.h"
-
-const uint32_t img_value = 0x12345678;
-void __attribute__((noinline)) flash_load_img(void){};
-
-void __attribute__((noinline)) boot_next_stage(void) {
-  // Indicate we successfully bypassed the signature verification
-  __SET_SIM_SUCCESS();
-}
+// Simple main program
 
 int main(void) {
-  serial_puts("Start Secure Boot...\n");
-  flash_load_img();
-
-  uint32_t *image = (uint32_t *)IMG_LOAD_ADDR;
-
-  if (memcmp(&img_value, image, sizeof(uint32_t))) {
-    serial_puts("Negative program flow!\n");
-    __SET_SIM_FAILED();
+  volatile int count = 0;
+  for (int i = 0; i < 1000; i++) {
+    count += i;
   }
-
-  serial_puts("Positive program flow\n");
-
-  boot_next_stage();
-
-  while (true) {
-  };
-
-  return 0;
+  return count;
 }
