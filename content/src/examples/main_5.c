287670c3b895c900b869498ba1ffbf21629f5fe3
Roland Ebrecht <roland.ebrecht@infineon.com.com>
2024-07-16 11:29:45 +0200

    Further improved example 5 (main_5.c ). Working with -O3 and -Os

--- content/src/examples/main_5.c
+++ content/src/examples/main_5.c
@@ -18,6 +18,16 @@ __attribute__((noinline)) fih_uint get_value(fih_uint *t) {
   return * t;
 }
 
+#define fih_uint_eq_new(x, y)  \
+  (fih_uint_validate(x)  && \
+    (get_value(&x).val == (y).val) && \
+    fih_delay() && \
+    (get_value(&x).msk == (y).msk) && \
+    fih_delay() && \
+    ((y).val == FIH_UINT_VAL_MASK(get_value(&x).msk))  \
+  )
+
+
 /*******************************************************************************
  * Function Name:  main
  *******************************************************************************
@@ -29,10 +39,10 @@ __attribute__((noinline)) fih_uint get_value(fih_uint *t) {
 
   serial_puts("Some code 1...\n");
 
-  if (fih_uint_eq_new(get_value(&DECISION_DATA), FIH_SUCCESS)) {
+  if (fih_uint_eq_new(DECISION_DATA, FIH_SUCCESS)) {
     serial_puts("Verification positive path : OK\n");
     
-    // Changed to "with_condition" because of linker problem
+    // Changed to "with_condition" because of linker problem (start_success_handling() is directly located after get_value()). 
     __SET_SIM_CONDITION_TRUE();
 
     start_success_handling();
