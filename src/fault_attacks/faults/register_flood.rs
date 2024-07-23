4026b157c1187338219ea454baf7511ae5b9e0f4
Roland Ebrecht <roland.ebrecht@infineon.com.com>
2024-07-23 11:49:11 +0200

    Added new attack: Register Flood (regflood), which sets or clear chosen register

---
+++ src/fault_attacks/faults/register_flood.rs
@@ -0,0 +1,125 @@
+use super::{Disassembly, FaultFunctions, FaultType};
+use crate::simulation::{
+    cpu::{Cpu, ARM_REG},
+    fault_data::FaultData,
+    record::{FaultRecord, TraceRecord},
+};
+use std::fmt::Debug;
+use std::sync::Arc;
+use unicorn_engine::RegisterARM;
+
+/// Register flood fault structure
+///
+#[derive(Clone, Copy)]
+pub struct RegisterFlood {
+    pub register: RegisterARM,
+    pub value: u32,
+}
+
+impl Debug for RegisterFlood {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "Register Flood (regflood_r{}_{:08x})",
+            self.register as u32 - RegisterARM::R0 as u32,
+            self.value
+        )
+    }
+}
+
+/// Implementation for Glitch fault
+impl RegisterFlood {
+    /// Create a new Glitch fault
+    pub fn new(register: RegisterARM, value: u32) -> Arc<Self> {
+        Arc::new(Self { register, value })
+    }
+}
+
+impl FaultFunctions for RegisterFlood {
+    /// Execute a bit flip in the given register.
+    fn execute(&self, cpu: &mut Cpu, fault: &FaultRecord) {
+        let address = cpu.get_program_counter();
+
+        // Read and write changed register
+        let reg_val = cpu.register_read(self.register).unwrap();
+        cpu.register_write(self.register, self.value as u64)
+            .unwrap();
+
+        // Read assembler line
+        let mut original_instructions = vec![0; cpu.get_asm_cmd_size(address).unwrap()];
+        // Read original instructions
+        cpu.memory_read(address, &mut original_instructions)
+            .unwrap();
+
+        let record = TraceRecord::Fault {
+            address,
+            fault_type: format!(
+                "Register Flood (Reg: R{}, Value: {:08x}) 0x{:08x} -> 0x{:08x}",
+                self.register as u32 - RegisterARM::R0 as u32,
+                self.value,
+                reg_val,
+                self.value as u64
+            ),
+        };
+        cpu.get_trace_data().push(record.clone());
+
+        // Push to fault data vector
+        cpu.get_fault_data().push(FaultData {
+            original_instructions,
+            record,
+            fault: fault.clone(),
+        });
+    }
+
+    /// Filtering of traces to reduce the number of traces to analyze
+    fn filter(&self, records: &mut Vec<TraceRecord>, cs: &Disassembly) {
+        records.retain(|record| match record {
+            TraceRecord::Instruction {
+                address,
+                asm_instruction,
+                ..
+            } => cs.check_for_register(
+                asm_instruction,
+                *address,
+                self.register as u32 - RegisterARM::R0 as u32,
+            ),
+            _ => false,
+        });
+    }
+
+    /// Try to parse a Glitch fault from a string
+    fn try_from(&self, input: &str) -> Option<FaultType> {
+        // divide name from attribute
+        let collect: Vec<&str> = input.split('_').collect();
+        // check if name and attribute are present
+        let fault_type = collect.first().copied()?;
+        let attribute_1 = collect.get(1).copied()?;
+        let attribute_2 = collect.get(2).copied()?;
+        // check if fault type is glitch
+        if fault_type == "regflood" {
+            // check if attribute is a register
+            if let Some(stripped) = attribute_1.strip_prefix('r') {
+                // check if attribute is a valid register
+                if let Ok(register) = stripped.parse::<usize>() {
+                    // check if attribute is a valid value
+                    if let Ok(xor_value) = u32::from_str_radix(attribute_2, 16) {
+                        // return Glitch struct
+                        return Some(Self::new(ARM_REG[register], xor_value));
+                    }
+                }
+            }
+        }
+        None
+    }
+    /// Get the list of possible/good faults
+    fn get_list(&self) -> Vec<String> {
+        let mut list = Vec::new();
+        // Generate a list of all possible register bitflips
+        // Values will look like: regbf_r0_000000000, regbf_r0_FFFFFFFF, ...
+        for reg in 0..=12 {
+            list.push(format!("regflood_r{}_{:08x}", reg, 0));
+            list.push(format!("regflood_r{}_{:08x}", reg, 0xFFFFFFFFu32));
+        }
+        list
+    }
+}
